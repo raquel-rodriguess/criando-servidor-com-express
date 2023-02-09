0c1361eeebadf2fcb5de0869c9b918210bc419ac
raquel <kellrodrigues055@gmail.com>
2023-02-08 23:50:42 -0300

    Adicionando arquivo sql

--- Arquivos sql/teste.sql
+++ Arquivos sql/teste.sql
@@ -1,5 +1,19 @@
+
 create table usuario (
     nome varchar(50),
     email varchar(100),
     idade int
 );
+
+INSERT INTO usuario (nome, email, idade) VALUES(
+    "jaila",
+    "jaila@teste.com",
+    23
+);
+    SELECT * FROM usuario  WHERE idade = 23;
+     SELECT * FROM usuario  WHERE idade >= 24;
+
+     DElETE FROM usuario WHERE idade= 26;
+      DElETE FROM usuario WHERE nome = "victor";
+
+      UPDATE usuario  SET nome = "Kell"  WHERE nome = "raquel";
